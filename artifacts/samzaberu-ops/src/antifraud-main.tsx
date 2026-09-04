3273ae0d79acbd1d20fa1f1501b7a101e2fb6151
juvantusik <61152884+juvantusik@users.noreply.github.com>
2026-09-04 20:19:35 +0400

    feat: add Anti-Fraud React entry

---
+++ artifacts/samzaberu-ops/src/antifraud-main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import AntiFraudPage from './pages/AntiFraudPage';
+import './pages/anti-fraud-web.css';
+
+// Добавлено 03.09.2026 ИТ Директор Евразии
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AntiFraudPage />
+  </React.StrictMode>,
+);
